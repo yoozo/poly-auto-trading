b330a117c248e914b2989e14599bc4c4f6a56580
yoozo <304738717@qq.com>
2026-06-26 22:34:27 +0800

    fix: allow poly auto dev host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    allowedHosts: ["sg.dev"],
+    allowedHosts: ["poly-auto.yoozo.xyz"],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
