25012b5cb468c0af61cd7e8ee663b06232df1f9d
yoozo <304738717@qq.com>
2026-06-27 11:00:30 +0800

    fix: update allowed dev host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    allowedHosts: ["poly-auto.yoozo.xyz"],
+    allowedHosts: ["auto-poly.yoozo.xyz"],
     proxy: {
       "/api": {
         target: "http://localhost:8000",
